58edb1a56c5b0c58e1293c0b6fafa161560c1f65
Murugan D <yasmuru@users.noreply.github.com>
2023-08-26 22:33:50 +0530

    Update to get all asset balances

--- app/views.py
+++ app/views.py
@@ -6,26 +6,27 @@ from django.http import JsonResponse
 
 
 async def get_balance(request):
-    facade = ChainFacade.node_provider_testnet()
+    facade = ChainFacade.node_provider_mainnet()
     neo = NeoToken()
     gas = GasToken()
     address = request.GET.get('address', None)
     token_list = []
-    try:
-        neo_balance = await facade.test_invoke(neo.balance_of(address))
-        gas_balance = await facade.test_invoke(gas.balance_of(address))
-    except Exception as e:
-        print(e)
+    # try:
+    #     neo_balance = await facade.test_invoke(neo.balance_of(address))
+    #     gas_balance = await facade.test_invoke(gas.balance_of(address))
+    # except Exception as e:
+    #     print(e)
         
-        response = []
-        return JsonResponse(response, safe=False)
-    
+    #     response = []
+    #     return JsonResponse(response, safe=False)
+    print(facade.rpc_host)
     # response['data'] = {'address': address, 'quote_currency': 'USD'}
-    neo_token = {'amount': neo_balance, 'contract_address': '0xef4073a0f2b305a38ec4050e4d3d28bc40ea63f5', 'symbol': 'NEO', 'decimals': neo.decimals()}
-    gas_token = {'amount': gas_balance, 'contract_address': '0xd2a4cff31913016155e38e474a2c06d08be276cf', 'symbol': 'GAS', 'decimals': gas.decimals()}
-    gas_token['price'] = 0
-    gas_token['value'] = 0
-    url = "http://seed4t5.neo.org:20332/"
+    # neo_token = {'amount': neo_balance, 'contract_address': '0xef4073a0f2b305a38ec4050e4d3d28bc40ea63f5', 'symbol': 'NEO', 'decimals': neo.decimals()}
+    # gas_token = {'amount': gas_balance, 'contract_address': '0xd2a4cff31913016155e38e474a2c06d08be276cf', 'symbol': 'GAS', 'decimals': gas.decimals()}
+    # gas_token['price'] = 0
+    # gas_token['value'] = 0
+    # url = "http://seed4t5.neo.org:20332/"
+    url="http://seed1.neo.org:10332"
 
     payload = json.dumps({
         "jsonrpc": "2.0",
@@ -56,8 +57,8 @@ async def get_balance(request):
                     if neo_json:=usd_json.get('neo', None):
                         usd_value = neo_json.get('usd', 0)
                         token['price'] = usd_value
-                        price = neo_balance * usd_value
-                        token['value'] = price
+                        # price = neo_balance * usd_value
+                        # token['value'] = price
             token_list.append(token)
     
     payload = json.dumps({
@@ -72,10 +73,14 @@ async def get_balance(request):
     if token_response.status_code in {200, 201}:
         token_json = token_response.json()
         for asset in token_json['result']['balance']:
-            contract_address = asset['asset_hash']
+            print(asset)
+            contract_address = asset['assethash']
             symbol = asset['symbol']
             decimals = asset['decimals']
-            amount = asset['amount']
+            amount = asset.get('amount', 0)
+            if asset.get('tokens', []):
+                for to in asset.get('tokens'):
+                    amount += float(to.get('amount', 0))
             token = {'amount': amount, 'contract_address': contract_address, 'symbol': symbol, 'decimals': decimals}
             if symbol.lower() == 'neo':
                 price_response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=neo&vs_currencies=usd')
@@ -84,8 +89,8 @@ async def get_balance(request):
                     if neo_json:=usd_json.get('neo', None):
                         usd_value = neo_json.get('usd', 0)
                         token['price'] = usd_value
-                        price = neo_balance * usd_value
-                        token['value'] = price
+                        # price = neo_balance * usd_value
+                        # token['value'] = price
             token_list.append(token)     
     response = token_list
     # response['status_code'] = 200
