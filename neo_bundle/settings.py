69cbff31a185250f236900c259f51a30afdd8453
Murugan D <yasmuru@users.noreply.github.com>
2023-08-26 23:27:30 +0530

    Avoid celery

--- neo_bundle/settings.py
+++ neo_bundle/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'django_mysql',
     'ckeditor',
     'ckeditor_uploader',
-    'django_celery_results',
-    'django_celery_beat',
+    # 'django_celery_results',
+    # 'django_celery_beat',
     'rest_framework',
     'storages',
     'xlwt',
@@ -200,13 +200,13 @@ MEDIA_URL = '/media/'
 AUTH_USER_MODEL = "app.User"
 
 # CELERY STUFF
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-# CELERY_TIMEZONE = 'Africa/Nairobi'
-CELERY_TIMEZONE = 'Asia/Calcutta'
+# BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# # CELERY_TIMEZONE = 'Africa/Nairobi'
+# CELERY_TIMEZONE = 'Asia/Calcutta'
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 CKEDITOR_UPLOAD_PATH = ''
